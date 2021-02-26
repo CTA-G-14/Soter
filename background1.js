2d2bd17fbfb48573c13994c0b1ee24356635f558
TheElliot-U <61678470+TheElliot-U@users.noreply.github.com>
2021-02-26 21:03:13 +0530

    Delete background1.js

--- background1.js
+++
@@ -1,8 +0,0 @@
-chrome.extension.onRequest.addListener(function(prediction){
-    if (prediction==1){
-        alert("Warning: Malicious website detected !!!");
-        alert("***If you want to block click on extension icon***");
-    }else if (prediction==-1){
-        alert("Website is SAFE");
-    }
-});
